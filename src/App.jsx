08048d6da28374a419ca417cae82052c880e5464
kapadam navya <kapadamnavya20@gmail.com>
2024-11-13 14:28:53 +0530

    addeded

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-
+import Footer from "./components/Footer"
 export default function App() {
   return (
     <div>
-      importing
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+function Footer() {
+const buttonStyle = {
+    backgroundColor: '#4CAF50',
+    color: "white",
+    padding: '15px 32px',
+    fontSize: '16px',
+    border: 'none',
+    borderRadius: '12px',
+    cursor: 'pointer'
+  };
+  const heading={
+    fontSize:'15px',
+    fontweight:'50',
+    color:'black',
+
+  };
+  const listing={
+    display:"flex",
+    justifycontent:"space-between",
+    alignitems:'center',
+  };
+  const liststyling={
+    liststyle:"none",
+  }
+
+
+    return (<>
+        <h2 style={buttonStyle}>hello this hedign</h2>
+        <div
+    </>
+    );
+}
+
+export default Footer;
