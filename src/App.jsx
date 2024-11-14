633dd83196e8f8575814fc004172b81a913b9eac
Juliet Joyce bhaskaran <b.julietjoyce@gmail.com>
2024-11-14 14:39:46 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 
 import Topbar from "./components/Topbar"
+import Navbar from "./components/Navbar"
 export default function App() {
   return (
     <div>
-    <Topbar/>
+      <Topbar/>
+    <Navbar/>
     </div>
   )
 }
