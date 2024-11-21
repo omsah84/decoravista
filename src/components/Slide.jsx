e5465c03d239ea8143d3bf96642d468fc55e1aa1
Ayyaluri Gayathri <ayyalurigayathrireddy@gmail.com>
2024-11-22 00:04:53 +0530

    added

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,26 +1,18 @@
 function Slide() {
     return (
         <>
-            <h5 className="text-center ">Your Trust Factor</h5>
+            <h5 style={hed} className="text-center">Your Trust Factor</h5>
             <div style={bodystyle} className="d-flex justify-content-center items-center">
                 <div style={divstyle}>
-                    <svg style={svg} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
-                        <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5" />
-                    </svg>
+                <img width="55" height="55" src="https://img.icons8.com/fluency/48/house-with-a-garden.png" alt="house-with-a-garden"/>
                     <p style={para}>100,000+happy homes</p>
                 </div>
                 <div style={divstyle1}>
-                    <svg style={svg} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-building" viewBox="0 0 16 16">
-                        <path d="M4 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zM4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM7.5 5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM4.5 8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5z" />
-                        <path d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1zm11 0H3v14h3v-2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V15h3z" />
-                    </svg>
+                <img width="55" height="55" src="https://img.icons8.com/fluency/48/building-with-rooftop-terrace.png" alt="building-with-rooftop-terrace"/>
                     <p style={para}>60+ cities</p>
                 </div>
                 <div style={divstyle2}>
-                    <svg style={svg} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pin-map-fill" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8z" />
-                        <path fill-rule="evenodd" d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999z" />
-                    </svg>
+                <img width="55" height="55" src="https://img.icons8.com/dotty/80/home-page.png" alt="home-page"/>
                     <p style={para}>3 countries</p>
                 </div>
                 <div style={divstyle3}>
@@ -30,63 +22,72 @@ function Slide() {
                     <p style={para}>20 lakh+catalogue <br></br>products</p>
                 </div>
                 <div style={divstyle4}>
-                    <svg style={svg} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-laptop-fill" viewBox="0 0 16 16">
-                        <path d="M2.5 2A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5" />
-                    </svg>
+                <img width="55" height="55" src="https://img.icons8.com/color/48/office.png" alt="office"/>
                     <p style={para}>3,500+designers</p>
                 </div>
             </div>
-            <div>
+            <div style={divv}>
                 <h4 className="text-center">Home Office to Match Every Style</h4>
-                <p className="text-center">Explore productive and creative spaces tailored to your needs</p>
+                <p style={divv1}className="text-center">Explore productive and creative spaces tailored to your needs</p>
             </div>
         </>
     )
+}
+const hed={
+    marginTop:"20px",
+    fontSize:"25px"
+}
+const divv={
+    marginTop:"50px",
+}
+const divv1={
+    marginTop:"20px",
+    fontWeight: "600"
 }
 const bodystyle = {
     fontSize: "17px",
     fontWeight: "700",
 }
 const divstyle = {
-    border: "1px solid black",
     width: "350px",
     height: "120px",
     marginLeft: "50px",
     marginRight: "20px",
     padding: "10px",
     textAlign: "center",
+    boxShadow:"8px 8px 5px #bccec4"
 }
 const divstyle1 = {
-    border: "1px solid black",
     width: "350px",
     height: "120px",
     marginRight: "20px",
     padding: "10px",
     textAlign: "center",
+    boxShadow:"8px 8px 5px #bccec4"
 }
 const divstyle2 = {
-    border: "1px solid black",
     width: "350px",
     height: "120px",
     marginRight: "20px",
+     boxShadow:"8px 8px 5px #bccec4",
     padding: "10px",
     textAlign: "center",
 }
 const divstyle3 = {
-    border: "1px solid black",
     width: "350px",
     height: "120px",
     marginRight: "20px",
     padding: "10px",
     textAlign: "center",
+     boxShadow:"8px 8px 5px #bccec4"
 }
 const divstyle4 = {
-    border: "1px solid black",
     width: "350px",
     height: "120px",
     marginRight: "40px",
     padding: "10px",
     textAlign: "center",
+     boxShadow:"8px 8px 5px #bccec4"
 
 }
 const svg = {
@@ -96,6 +97,7 @@ const svg = {
 }
 const para = {
     padding: "13px",
+    fontSize:"15px"
 }
 
 
