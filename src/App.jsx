7ebe6ec2e7c053dc4e8539effb27693958d51c6f
Ayyaluri Gayathri <ayyalurigayathrireddy@gmail.com>
2024-11-21 15:25:18 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import Slide from "./components/Slide"
-export default function App() {
+import Slide from "./components/Slide";
+import React from "react";
+import Slider from "./components/Slider"
+
+function App() {
   return (
     <div>
       <Slide/>
+      <Slider/>
     </div>
-  )
+  );
 }
+
+export default App;
